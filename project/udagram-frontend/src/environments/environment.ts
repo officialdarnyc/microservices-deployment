0aa13abad36774f3565e49f4e17e69ce4e64da3d
Ikenna <ikenna.chiakwa@outlook.com>
2021-11-25 05:55:15 +0100

    rebuild and retag frontend

--- project/udagram-frontend/src/environments/environment.ts
+++ project/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9728df5cc3d34a42a97ab6b9f32aefd-653896227.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
